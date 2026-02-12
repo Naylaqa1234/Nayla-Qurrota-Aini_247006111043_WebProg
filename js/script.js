7f9e6b68dca37d979518bd1f7b696bbc76c3a1a7
Nayla Qurrota <naylaqurrota14@gmail.com>
2026-02-12 21:53:08 +0700

    ubah ukuran foto + Spotlight effect

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 function showMessage() {
   alert("Terima kasih telah mengunjungi portofolio saya!");
 }
+
+const hero = document.querySelector(".hero");
+
+hero.addEventListener("mousemove", (e) => {
+  const rect = hero.getBoundingClientRect();
+  hero.style.setProperty("--x", e.clientX - rect.left + "px");
+  hero.style.setProperty("--y", e.clientY - rect.top + "px");
+});
+
+hero.addEventListener("mousemove", (e) => {
+  const light = hero.querySelector("::before");
+});
+
